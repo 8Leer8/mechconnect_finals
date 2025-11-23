8070ed592e3f148cb66ce9bc8ab61a7f3832e15e
Mard <bengilreymard@gmail.com>
2025-11-23 10:58:10 +0800

    change from vanilla to react ionic

---
+++ frontend/mechconnect/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mechconnect',
+  webDir: 'dist'
+};
+
+export default config;
